b9876337c97ebaee9b44cccaa639c0f339a70e06
mouni364 <60179224+mouni364@users.noreply.github.com>
2020-05-18 17:06:28 +0530

    navigation changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,15 @@ import { Router, NavigationStart, ActivatedRoute } from '@angular/router';
 })
 export class AppComponent {
   title = 'EAR';
+  previous: String;
 
   totalUrl = new Map();
 
   currentOrderMappingUrls = new Map();
 
-  constructor(private router: Router,private route: ActivatedRoute) {
+  constructor(private router: Router, private route: ActivatedRoute) {
     this.totalUrlMapping();
-    this.projectDirectoryNavigation(this.router,this.route);
+    this.projectDirectoryNavigation(this.router, this.route);
   }
 
   totalUrlMapping(): void {
@@ -27,26 +28,42 @@ export class AppComponent {
   }
 
 
-  projectDirectoryNavigation(router: Router,route : ActivatedRoute) {
+  projectDirectoryNavigation(router: Router, route: ActivatedRoute) {
     let entry: any;
-    let id:any;
+    let id: any;
     let isMatch: Boolean = false;
+
     router.events.subscribe(event => {
       if (event instanceof NavigationStart) {
-        id = this.route.snapshot.params['id'];
+
         this.totalUrlMapping();
         if (event.url == '/docrepo') {
           this.totalUrl.clear();
           this.totalUrl.set(0, ["/folder", "DashBoard"]);
           this.totalUrl.set(1, ["/docrepo", "Document Repository"]);
           this.SplitProjectDirectory(event, this.totalUrl)
-        } else if (event.url.split("/",2)[1] == 'clientdetails') {
+        } else if (event.url.split("/", 2)[1] == 'clientdetails') {
           this.totalUrl.clear();
           this.totalUrl.set(0, ["/folder", "DashBoard"]);
           this.totalUrl.set(1, [event.url, "ClientDetails"]);
           this.SplitProjectDirectory(event, this.totalUrl)
+        } else if (this.previous == '/docrepo' && (event.url == '/project' || event.url == '/preview')) {
+          this.totalUrl.set(0, ["/folder", "DashBoard"]);
+          this.totalUrl.set(1, ["/docrepo", "Document Repository"]);
+          this.totalUrl.set(2, ["/project", "Project Details"]);
+          this.totalUrl.set(3, ["/brd", "Business Requirement"]);
+          this.totalUrl.set(4, ["/preview", "Document Preview"]);
+          this.SplitProjectDirectory(event, this.totalUrl);
+        } else if (this.previous == '/docrepo' && event.url == '/brd') {
+          this.totalUrl.set(0, ["/folder", "DashBoard"]);
+          this.totalUrl.set(1, ["/docrepo", "Document Repository"]);
+          this.totalUrl.set(3, ["/brd", "Business Requirement"]);
+          this.totalUrl.set(4, ["/preview", "Document Preview"]);
+          this.SplitProjectDirectory(event, this.totalUrl);
         } else {
           isMatch = false;
+
+
           for (let newEntry of this.totalUrl.entries()) {
             if (event.url == newEntry[1][0]) {
               isMatch = true;
@@ -54,6 +71,7 @@ export class AppComponent {
             }
           }
           if (!isMatch) {
+            console.log(isMatch);
             if (event.url !== "/") {
               event.url = "/folder"
               this.totalUrl.set(0, ["/folder", "DashBoard"]);
@@ -74,10 +92,25 @@ export class AppComponent {
   SplitProjectDirectory(event: any, totalMap: any): void {
     if (event.url.length > 1) {
       this.currentOrderMappingUrls.clear();
+     
+     
+      
       for (let newEntry of totalMap.entries()) {
         if (event.url == newEntry[1][0]) {
-          this.currentOrderMappingUrls.set(newEntry[0], totalMap.get(newEntry[0]));
+          // this.currentOrderMappingUrls.set(newEntry[0], totalMap.get(newEntry[0]));
+          if (newEntry[1][0] == '/docrepo') {
+            this.previous = newEntry[1][0];
+          }
+          if (newEntry[1][0] == '/client') {
+            this.previous = newEntry[1][0];
+          }
           break;
+        }
+        if (newEntry[1][0] == '/docrepo') {
+          this.previous = newEntry[1][0];
+        }
+        if (newEntry[1][0] == '/client') {
+          this.previous = newEntry[1][0];
         }
         this.currentOrderMappingUrls.set(newEntry[0], totalMap.get(newEntry[0]));
       }
